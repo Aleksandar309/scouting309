a297f58f14ba5f23a01999a0d9216cabcae2f961
[dyad] <git@dyad.sh>
2025-12-03 20:08:19 +0100

    [dyad] Promenjena boja pozadine trake napretka u atributima igrača - wrote 1 file(s)

--- src/components/AttributeRating.tsx
+++ src/components/AttributeRating.tsx
@@ -15,11 +15,11 @@ const AttributeRating: React.FC<AttributeRatingProps> = ({ name, rating, classNa
     <div className={cn("flex items-center justify-between py-1", className)}>
       <span className="text-sm text-gray-300 w-1/2">{name}</span>
       <div className="flex items-center w-1/2">
-        {/* Postavljamo pozadinu trake na tamno sivu (bg-gray-700) i boju ispune na belu (bg-white) za jasan kontrast */}
+        {/* Postavljamo pozadinu trake na svetliju sivu (bg-gray-500) i boju ispune na belu (bg-white) za jasan kontrast */}
         <Progress 
           value={progressValue} 
-          className="h-2 w-full bg-gray-700" // Pozadina trake
-          indicatorClassName="bg-white" // Boja ispune
+          className="h-2 w-full bg-gray-500" // Pozadina trake je sada svetlija siva
+          indicatorClassName="bg-white" // Boja ispune ostaje bela
         />
         <span className="ml-2 text-sm text-gray-300">{rating}</span>
       </div>
